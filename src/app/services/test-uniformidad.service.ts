d957df01c91e0c8c3829b2ddde19311ad5fbfc9d
Jonatan Garcia Schaff <joni@darkstar.example.net>
2018-03-26 20:59:59 -0300

    primer commit joni

--- src/app/services/test-uniformidad.service.ts
+++ src/app/services/test-uniformidad.service.ts
@@ -1,70 +1,71 @@
-import { Injectable } from '@angular/core';
-import { RandomNumbersService } from '../services/random-numbers.service';
+import {Injectable} from '@angular/core';
+import {RandomNumbersService} from '../services/random-numbers.service';
 
 @Injectable()
 export class TestUniformidadService {
 
-  	constructor(private randomNumbersService: RandomNumbersService) {
-		
-   	}
-	
-	uniformityTest(valores_iniciales): void {
+  constructor(private randomNumbersService: RandomNumbersService) {
 
-            let a = 0;
-            let k = valores_iniciales['k'];
-            let randomNumber = 0;
-            let subInterval = 1 / k;
-            let subIntervalTemp = 0 - subInterval;
-            let subIntervals: Array<any> = [
-                [0 , 1, 'frecuencia']
-            ];
-            
-            for(a = 0 ; a < k; a++) {
-                subIntervalTemp = subIntervalTemp + subInterval;
-                subIntervals[a] = [subIntervalTemp, subIntervalTemp + subInterval, 0 ];
-            }
-            
-            for(let i = 0 ; i < this.randomNumbersService.sequences[0].length; i++) {
-                randomNumber = this.randomNumbersService.sequences[0][i];
-                subIntervals.forEach((element) => {
-                    if (randomNumber >= element[0] && randomNumber < element[1]) {
-                        element[2]++;
-                    }
-                });
-            } 
-            
-            // Obtengo N.
-            let n = valores_iniciales['numbersQuant'];
+  }
 
-            var sumatoria_1 = 0;
+  uniformityTest(valores_iniciales): void {
 
-            // Recorro los subintervalos.
-            subIntervals.forEach((element) => {
-                
-                // Obtengo la frecuencia.
-                let frecuencia = element[2];
-                
-                let resultado_1 =  frecuencia - (n/k);
+    let a = 0;
+    let k = valores_iniciales['k'];
+    let randomNumber = 0;
+    let subInterval = 1 / k;
+    let subIntervalTemp = 0 - subInterval;
+    let subIntervals: Array<any> = [
+      [0, 1, 'frecuencia']
+    ];
 
-                let resultado_2 = resultado_1 * resultado_1;
-
-                sumatoria_1 = sumatoria_1 + resultado_2;
-
-            });
-
-            let chi_cuadrado_calculado = sumatoria_1 * (k/n);
-
-			let rta:any = {
-				'Estado': true,
-				'subIntervals': subIntervals,
-				'chi_cuadrado_calculado': chi_cuadrado_calculado
-			}
-
-            return rta 
-        
-        /*subIntervals.forEach((element) => {
-            document.write('Intervalo: ' + element[0] + ' - ' + element[1] + '/////// FRECUENCIA :' + element[2] + '<br>');
-        }); */
+    for (a = 0; a < k; a++) {
+      subIntervalTemp = subIntervalTemp + subInterval;
+      subIntervals[a] = [subIntervalTemp, subIntervalTemp + subInterval, 0];
     }
+
+    for (let i = 0; i < this.randomNumbersService.sequences[0].length; i++) {
+      randomNumber = this.randomNumbersService.sequences[0][i];
+      subIntervals.forEach((element) => {
+        if (randomNumber >= element[0] && randomNumber < element[1]) {
+          element[2]++;
+        }
+      });
+    }
+
+    // Obtengo N.
+    let n = valores_iniciales['numbersQuant'];
+
+    var sumatoria_1 = 0;
+
+    // Recorro los subintervalos.
+    subIntervals.forEach((element) => {
+
+      // Obtengo la frecuencia.
+      let frecuencia = element[2];
+
+      let resultado_1 = frecuencia - (n / k);
+
+      let resultado_2 = resultado_1 * resultado_1;
+
+      sumatoria_1 = sumatoria_1 + resultado_2;
+
+    });
+
+    let chi_cuadrado_calculado = sumatoria_1 * (k / n);
+
+    //Creo el diccionario de respuesta.
+    let rta: any = {
+      'Estado': true,
+      'subIntervals': subIntervals,
+      'chi_cuadrado_calculado': chi_cuadrado_calculado
+    };
+
+    return rta
+
+    /*subIntervals.forEach((element) => {
+     document.write('Intervalo: ' + element[0] + ' - ' + element[1] + '/////// FRECUENCIA :' + element[2] + '<br>');
+     }); */
+  }
 
 }
